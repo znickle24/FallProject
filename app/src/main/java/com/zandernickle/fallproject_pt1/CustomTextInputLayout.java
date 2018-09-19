af82f46f509e33cde2a5a744346c4b592e036fc3
Matt Josse <matt.m.josse@gmail.com>
2018-09-19 16:31:45 -0600

    initial commit

---
+++ app/src/main/java/com/zandernickle/fallproject_pt1/CustomTextInputLayout.java
@@ -0,0 +1,34 @@
+package com.zandernickle.fallproject_pt1;
+
+import android.content.Context;
+import android.support.design.widget.TextInputLayout;
+import android.util.AttributeSet;
+
+/**
+ * A custom implementation of TextInputLayout. Extending TextInputLayout, rather than creating a
+ * few methods in a utility class, provides better readability, ease of access, and most
+ * importantly, extensibility for future features.
+ */
+public class CustomTextInputLayout extends TextInputLayout {
+
+    public CustomTextInputLayout(Context context) {
+        super(context);
+    }
+
+    public CustomTextInputLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public String getEditTextString() {
+        return getEditText().getText().toString();
+    }
+
+    public void showError(String message) {
+        setError(message);
+        setErrorEnabled(true);
+    }
+}
