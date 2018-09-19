845c7c29192f6579daaebc291e811eeb05e0e480
Matt Josse <matt.m.josse@gmail.com>
2018-09-19 14:24:52 -0600

    updated bitmapToBundle and added bitmapFromBundle

--- app/src/main/java/com/zandernickle/fallproject_pt1/ReusableUtil.java
+++ app/src/main/java/com/zandernickle/fallproject_pt1/ReusableUtil.java
@@ -3,6 +3,7 @@ package com.zandernickle.fallproject_pt1;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -38,11 +39,40 @@ public class ReusableUtil {
         return image;
     }
 
-    public static void bitmapToBundle(Bundle outState, Bitmap bitmap, String key) {
+    /**
+     * Compresses a Bitmap to a ByteArray and saves it to a Bundle. Intended for use with the
+     * ReusableUtil.bitmapFromBundle method or where a ByteArray is to be decoded to a Bitmap
+     * after extraction from the Bundle.
+     *
+     * @param bundle the Bundle object to which to save the compressed Bitmap.
+     * @param bitmap the Bitmap to compress (convert to ByteArray) and save to the Bundle.
+     * @param key the Key to access the compressed Bitmap from the Bundle.
+     */
+    public static void bitmapToBundle(Bundle bundle, Bitmap bitmap, String key) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        // The quality argument is functionally irrelevant for PNG files (loss-less compression).
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
         byte[] compressedImage = outputStream.toByteArray();
-        outState.putByteArray(Key.PROFILE_IMAGE, compressedImage);
+        bundle.putByteArray(key, compressedImage);
+    }
+
+    /**
+     * Extracts a compressed Bitmap from a Bundle. Intended for use with the
+     * ReusableUtil.bitmapToBundle method or where the Bitmap is to be decoded from a ByteArray.
+     *
+     * This method will fail if attempting to extract a Bitmap directly from the Bundle. The
+     * Bitmap must have been saved to the Bundle as a ByteArray. If the Bundle does not contain
+     * the specified key, the returned Bitmap will be null.
+     *
+     * @param bundle the Bundle object from which to extract the compressed Bitmap.
+     * @param key the Key to access the compressed Bitmap from the Bundle.
+     * @return the Bitmap
+     */
+    @Nullable
+    public static Bitmap bitmapFromBundle(Bundle bundle, String key) {
+        byte[] compressedImage = bundle.getByteArray(key);
+        return BitmapFactory.decodeByteArray(compressedImage, 0, compressedImage.length);
     }
 
     /**
