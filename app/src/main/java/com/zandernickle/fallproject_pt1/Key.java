9364117504b0e5fbdc094d96cdd881af564f7fd1
Matt Josse <matt.m.josse@gmail.com>
2018-09-20 19:30:14 -0600

    updated value types and added a couple key-value pairs

--- app/src/main/java/com/zandernickle/fallproject_pt1/Key.java
+++ app/src/main/java/com/zandernickle/fallproject_pt1/Key.java
@@ -1,32 +1,34 @@
 package com.zandernickle.fallproject_pt1;
 
+/**
+ * Constants used to pass data throughout the app. Append to this list any keys (or tags/ids) not
+ * bounded by a single class.
+ */
 public class Key {
 
-    /* MATT
-     *
-     * The following keys represent any data which might be passed from the on-boarding
-     * activity(s) and/or the profile/account fragment. I've included a comment for each com.zandernickle.fallproject_pt1.Key
-     * specifying the type of data to expect when retrieving the key's corresponding value. See
-     * the enum files (or autocomplete) for all possible enum values.
-     */
+    // SignInFragment <-> MainActivity <-> FitnessInputFragment
     public static final String NAME = "NAME"; // String
     public static final String PROFILE_IMAGE = "PROFILE_IMAGE"; // String
     public static final String AGE = "AGE"; // int
-    public static final String POSTAL_CODE = "POSTAL_CODE"; // String
-    public static final String COUNTRY = "COUNTRY"; // String
-    public static final String HEIGHT = "HEIGHT"; // int (inches)
-    public static final String WEIGHT = "WEIGHT"; // int (lbs)
-    public static final String SEX = "SEX"; // enum (com.zandernickle.fallproject_pt1.Sex)
-    public static final String ACTIVITY_LEVEL = "ACTIVITY_LEVEL"; // enum (com.zandernickle.fallproject_pt1.ActivityLevel)
-    public static final String GOAL = "GOAL"; // enum (com.zandernickle.fallproject_pt1.Goal)
-    public static final String WEIGHT_GOAL = "WEIGHT_GOAL"; //int (lbs or kg)
+    public static final String POSTAL_CODE = "POSTAL_CODE"; // int
+    public static final String COUNTRY = "COUNTRY"; // enum (CountryCode)
+
+    // FitnessInputFragment <-> MainActivity <-> BMRFragment
+    public static final String HEIGHT = "HEIGHT"; // int (units defined by Key.COUNTRY)
+    public static final String WEIGHT = "WEIGHT"; // int (units defined by Key.COUNTRY)
+    public static final String SEX = "SEX"; // enum (Sex)
+    public static final String ACTIVITY_LEVEL = "ACTIVITY_LEVEL"; // enum (ActivityLevel)
+    public static final String GOAL = "GOAL"; // enum (Goal)
+    public static final String WEIGHT_GOAL = "WEIGHT_GOAL"; // int (units defined by Key.COUNTRY)
+
+    // TODO:
     public static final String BMR = "BMR"; //int (num kCal per day a person needs to maintain weight)
     public static final String BMI = "BMI"; //int
 
-    /* MATT
-     *
-     * Reusable constants.
-     */
-    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    // Used as both keys & tags
+    private static final String SIGN_IN_FRAGMENT = "SIGN_IN_FRAGMENT";
+    private static final String FITNESS_INPUT_FRAGMENT = "FITNESS_INPUT_FRAGMENT";
 
+    // Misc
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
 }
