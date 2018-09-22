40592a8991d048526d68c32200aec26b0a53110d
Matt Josse <matt.m.josse@gmail.com>
2018-09-22 08:04:57 -0600

    updates to support master list header fragment

---
+++ app/src/main/java/com/zandernickle/fallproject_pt1/MasterListHeaderFragment.java
@@ -0,0 +1,31 @@
+package com.zandernickle.fallproject_pt1;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class MasterListHeaderFragment extends Fragment {
+
+
+    public MasterListHeaderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_master_list_header, container, false);
+    }
+
+}
