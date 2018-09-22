62eba990397a43d9e8e02ea9e1615f4cce123ac1
Matt Josse <matt.m.josse@gmail.com>
2018-09-21 23:19:45 -0600

    added menu bar fragment

---
+++ app/src/main/java/com/zandernickle/fallproject_pt1/MenuBarFragment.java
@@ -0,0 +1,26 @@
+package com.zandernickle.fallproject_pt1;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MenuBarFragment extends Fragment {
+
+
+    public MenuBarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_bar, container, false);
+    }
+
+}
