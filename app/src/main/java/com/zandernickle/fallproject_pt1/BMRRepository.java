6ffeffa00cf31da3a9b81538a7fbb901aea7ce37
Zander Nickle <znickle24@icloud.com>
2018-10-10 12:21:07 -0600

    Just adding in the BMRRepo and the BMR View Model.

---
+++ app/src/main/java/com/zandernickle/fallproject_pt1/BMRRepository.java
@@ -0,0 +1,8 @@
+package com.zandernickle.fallproject_pt1;
+
+/**
+ * Created by znickle on 10/10/18.
+ */
+
+public class BMRRepository {
+}
---
+++ app/src/main/java/com/zandernickle/fallproject_pt1/BMRViewModel.java
@@ -0,0 +1,10 @@
+package com.zandernickle.fallproject_pt1;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by znickle on 10/10/18.
+ */
+
+public class BMRViewModel extends ViewModel {
+}
