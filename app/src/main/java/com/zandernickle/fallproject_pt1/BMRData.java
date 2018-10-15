a09acdc880dd6b27ec0db6cf15c424421b772ac7
Zander Nickle <znickle24@icloud.com>
2018-10-15 11:06:54 -0600

    Added BMRData and BMRRepo.

---
+++ app/src/main/java/com/zandernickle/fallproject_pt1/BMRData.java
@@ -0,0 +1,22 @@
+package com.zandernickle.fallproject_pt1;
+
+/**
+ * Created by znickle on 10/10/18.
+ */
+
+public class BMRData {
+    private int BMR = -1;
+    private int BMI = -1;
+    public int getBMR() {
+        return BMR;
+    }
+    public void setBMR(int _bmr) {
+        this.BMR = _bmr;
+    }
+    public int getBMI() {
+        return BMI;
+    }
+    public void setBMI(int _bmi) {
+        this.BMI = _bmi;
+    }
+}
--- app/src/main/java/com/zandernickle/fallproject_pt1/BMRRepository.java
+++ app/src/main/java/com/zandernickle/fallproject_pt1/BMRRepository.java
@@ -1,8 +1,14 @@
 package com.zandernickle.fallproject_pt1;
 
+import android.arch.lifecycle.MutableLiveData;
+
 /**
  * Created by znickle on 10/10/18.
  */
 
 public class BMRRepository {
+    private final MutableLiveData<BMRData> bmrData = new MutableLiveData<BMRData>();
+    private int BMR;
+    private int BMI;
+
 }
